d5fa96430c699fa3c9d3c735e6f240bcf3a8bf1a
Vee <happierv@gmail.com>
2014-06-17 19:04:40 +0900

    Finish /location/0 (report level)

--- app2.js
+++ app2.js
@@ -35,6 +35,14 @@ function location(req, res, next) {
         return next();
       });
     }
+    else if(level == '0'){
+      res.send(locationReport(reports));
+      return next();
+    }
+    else{
+      res.send([]);
+      return next();
+    }
   });
 }
 function locationCountry(reports){
@@ -52,14 +60,15 @@ function locationCountry(reports){
         }
       }
       if(mood_num > 0) mood_avg = Math.round(mood_sum/mood_num);
-      output.push({
-        'id': cc,
-        'name': countries.list[cc]['name'],
-        'lat': countries.list[cc]['lat'],
-        'lon': countries.list[cc]['lon'],
-        'mood': mood_avg,
-        'level': 2,
-      });
+      if(mood_avg && countries.list[cc]['lat'] && countries.list[cc]['lon'])
+        output.push({
+          'id': cc,
+          'name': countries.list[cc]['name'],
+          'lat': countries.list[cc]['lat'],
+          'lon': countries.list[cc]['lon'],
+          'mood': mood_avg,
+          'level': 2,
+        });
     }
   }
   return output;
@@ -81,14 +90,15 @@ function locationCity(reports, callbackLocation){
       }
       if(mood_num > 0) mood_avg = Math.round(mood_sum/mood_num);
       getCityLatLong(city, function(result){
-        output.push({
-          'id': city,
-          'name': result.name,
-          'lat': result.lat,
-          'lon': result.lon,
-          'mood': mood_avg,
-          'level': 1,
-        });
+        if(mood_avg && result.lat && result.lon)
+          output.push({
+            'id': city,
+            'name': result.name,
+            'lat': result.lat,
+            'lon': result.lon,
+            'mood': mood_avg,
+            'level': 1,
+          });
         callback();
       });
     } 
@@ -99,6 +109,29 @@ function locationCity(reports, callbackLocation){
     callbackLocation(output);
   });
 }
+function locationReport(reports){
+  var output = [];
+  for(i in reports){
+    if(reports[i].skyfeel && reports[i].lat != '0.000000' && reports[i].lon != '0.000000'){
+      output.push({
+        'id': reports[i].repoid,
+        'name': reports[i].name+" @ "+reports[i].location,
+        'lat': reports[i].lat,
+        'lon': reports[i].lon,
+        'mood': reports[i].skyfeel,
+        'level': 0
+      });
+    }
+    // else{
+    //   console.log(reports[i]);
+    // }
+  }
+  return output;
+}
+/*
+ * Try to get a lat/lon from the database, 
+ * and send a request if not found
+ */
 function getCityLatLong(cityName, callback){
   db.find({id: cityName}, function(err, docs){
     if(docs.length > 0){
@@ -113,7 +146,10 @@ function getCityLatLong(cityName, callback){
     }
   });
 }
-
+/*
+ * Send a request to openstreetmap to ask 
+ * for lat/lon and save it to the database
+ */
 function requestCityAndInsert(cityName, callback){
   request({
     uri: urlForGeocode(cityName),
@@ -145,7 +181,6 @@ function requestCityAndInsert(cityName, callback){
     });
   });
 }
-
 function getData(callback){
   request({
     uri: 'http://gsp.weathernews.jp/grpt6aa/gsp/tools/get_report_list.cgi',
@@ -159,11 +194,9 @@ function getData(callback){
     callback(obj);
   });
 }
-
 function urlForReverseGeocode (lat, lng) {
   return "http://nominatim.openstreetmap.org/reverse?lat=" + lat +"&lon=" + lng + "&accept-language=en-us&format=json&zoom=18";
 }
-
 function urlForGeocode (str) {
   return "http://nominatim.openstreetmap.org/search?q="+ str +"&format=json&accept-language=en-us&limit=1"
 }
